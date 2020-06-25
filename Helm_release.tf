021e389ca5dda56f7bb100017f555b2446c41468
George Smirnoff <georgi_1995@mail.ru>
2020-06-25 11:35:30 +0300

    uncomment helm release

--- Helm_release.tf
+++ Helm_release.tf
@@ -1,12 +1,12 @@
 data "helm_repository" "stable" {
   name = "stable"
 #  url  = "https://kubernetes-charts.storage.googleapis.com"
-  url  = "https://github.com/George-Smirnoff/helm_release/"
+  url  = "https://raw.githubusercontent.com/George-Smirnoff/helm_release/master"
 }
 
-/* resource "helm_release" "k8s_dashboard" {
+resource "helm_release" "k8s_cni" {
   namespace  = "kube-system"
-  name       = "kubernetes-dashboard"
+  name       = "kubernetes-cni"
   repository = data.helm_repository.stable.metadata[0].name
-  chart      = "kubernetes-dashboard"
-} */
+  chart      = "kubernetes-cni"
+}
